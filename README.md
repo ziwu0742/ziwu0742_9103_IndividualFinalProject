438eeb3838f2926cfe03d3d7c0815993b15f5693
ziwu0742 <ziwu0742@uni.sydney.edu.au>
2024-11-07 22:09:09 +1100

    Added sin and cos wave of Base Layer
    Use movementOffset and Oscillation

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # ziwu0742_9103_IndividualFinalProject
-#Individual Work Overview#
 
 ## Individual Work Overview
 For this individual work, I chose to apply the coding skills I acquired in this course to make certain elements of the artwork interactive, allowing them to _respond_ to different types of timing events
 ### Header 3
-Introduction of the Functions
+
+Instructions on how to interact with the work; e.g. move the mouse slowly over the screen, click the play button and wait for the music to start, load the page and the animation will happen over x number of seconds or whatever is needed to make your code come alive.
+Details of your individual approach to animating the group code.
+Which did you choose to drive your individual code: audio, interaction, Perlin noise or time.
+Which properties of the image will be animated and how; highlighting how it is unique from other group members (i.e. one changes colours, the other component sizes, another reveals only some components at a time, etc.). You will need to work with your group members to make sure your work is sufficiently different from each other.
+References to inspiration for animating your individual code; these can be images (still or gifs). How did they influence your submission?
+A short technical explanation of how your individual code works to animate the image and any appropriate references.
+If you made a lot of changes to the group code, explain it here.
+If you use tools and technique from outside the course, explain why you used them and how they work.
+If you copy a technique from the Internet, explain how it works, why you used it, and where it came from.
--- p5_project/sketch.js
+++ p5_project/sketch.js
@@ -1,24 +1,29 @@
-//Storing the line groups into an array.
+//Storing the line groups into an array. 
 let groups = [];
 let colorMiddle; // Color for the middle layer.
 let colorTop; // Color variable for the top layer
 let fadeAlphaMiddle = 255; // Initial alpha for middle layer color
-let value = 0;
+let triangleSize;
+let targetTriangleSize;
+
+// Set the easing value to a constant
+const easing = 0.05;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
-
+  
   colorMiddle = color(random(145, 188), 145, 188, fadeAlphaMiddle);
   colorTop = color(random(188, 255), 188, 255);
   colorBase = color(random(98, 126), 98, 126); // Base color without fade
 
   drawLineGroups(); // Initial draw of the base layer
   
-  setInterval(colorTopChange, 500);
-  
-  // Start the fade-out effect for the middle layer after a delay
-  setTimeout(fadeOutMiddleLayer, 2500);
+  setInterval(colorTopChange, 1000); // Start the color change effect for the top layer every 1 second
+  setTimeout(fadeOutMiddleLayer, 2500);  // Start the fade-out effect for the middle layer after a delay
+
+  triangleSize = random(height);
+  targetTriangleSize = triangleSize / 2;
 }
 
 function colorTopChange() {
@@ -27,54 +32,60 @@ function colorTopChange() {
 
 function fadeOutMiddleLayer() {
   if (fadeAlphaMiddle > 0) {
-    fadeAlphaMiddle -= 5;
-    fadeAlphaMiddle = max(fadeAlphaMiddle, 0); // Ensure alpha does not go below 0
-
-    // Update colorMiddle with the new alpha value
-    colorMiddle = color(red(colorMiddle), green(colorMiddle), blue(colorMiddle), fadeAlphaMiddle);
-
-    // Schedule the next fade step
-    setTimeout(fadeOutMiddleLayer, 50); // Adjust delay for fade speed
+	fadeAlphaMiddle -= 5;
+	fadeAlphaMiddle = max(fadeAlphaMiddle, 0); // Ensure alpha does not go below 0
+	
+	  // Update colorMiddle with the new alpha value
+	colorMiddle = color(red(colorMiddle), green(colorMiddle), blue(colorMiddle), fadeAlphaMiddle); 
+	
+	// Schedule the next fade step
+	setTimeout(fadeOutMiddleLayer, 50); // Adjust delay for fade speed
   }
 }
-
 //This class draws groups of lines using a class.
 //In this class, the start points'X location, start points'Y location, end points'X location, end points'Y location, angle, number of lines in each group, color, space, strokeWeight
 //are used as parameters.
 class lineGroup {
-  constructor(startX, startY, endX, endY, angle, numLines, color, space, strokeWeight) {
-    this.startX = startX;
-    this.startY = startY;
-    this.endX = endX;
-    this.endY = endY;
-    this.angle = angle;
-    this.numLines = numLines;
-    this.color = color;
-    this.lineSpacing = space; 
-    this.strokeWeight = strokeWeight;
+  constructor(startX, startY, endX, endY, angle, numLines, color, space, strokeWeight, movementOffset) {
+	this.startX = startX;
+	this.startY = startY;
+	this.endX = endX;
+	this.endY = endY;
+	this.angle = angle;
+	this.numLines = numLines;
+	this.color = color;
+	this.lineSpacing = space;
+	this.strokeWeight = strokeWeight;
+	this.movementOffset = movementOffset; // New parameter for movement
   }
 
-    draw() {
-    stroke(this.color); // Set the color of the lines.
-    strokeWeight(this.strokeWeight); // Set the line thickness.
-
-    // Draw each line in the group at an offset based on line spacing and angle.
-    for (let i = 0; i < this.numLines; i++) {
-      // Calculate horizontal and vertical offsets for each line.
-      let offsetX = cos(this.angle) * this.lineSpacing * i;
-      let offsetY = sin(this.angle) * this.lineSpacing * i;
-
-      // Draw the line with the calculated offsets.
-      line(this.startX + offsetX, this.startY + offsetY, this.endX + offsetX, this.endY + offsetY);
-    }
+  draw() {
+	stroke(this.color); // Set the color of the lines.
+	strokeWeight(this.strokeWeight); // Set the line thickness.
+	
+	// Draw each line in the group at an offset based on line spacing and angle.
+	for (let i = 0; i < this.numLines; i++) {
+	  // Offset calculations include oscillation effect
+	  let offsetX = cos(this.angle) * this.lineSpacing * i;
+	  let offsetY = sin(this.angle) * this.lineSpacing * i;
+	
+	  // Apply oscillation to the start and end points
+	  let xOscillation = sin(frameCount * 0.05 + i * this.movementOffset) * 5; // Adjust multiplier for more/less movement
+	  let yOscillation = cos(frameCount * 0.05 + i * this.movementOffset) * 5;
+	
+	  line(
+	    this.startX + offsetX + xOscillation, this.startY + offsetY + yOscillation,
+	    this.endX + offsetX + xOscillation, this.endY + offsetY + yOscillation
+	  );
+	}
   }
 }
 
 function draw() {
-  background(255);
+  background(50);
 
   for (let group of groups) {
-    group.draw();
+	group.draw();
   }
 
   drawMiddleLayer(); // Draw middle layer with fading color
@@ -102,9 +113,9 @@ function drawLineGroups() {
   let baseLayer3EndY = 0.285 * windowHeight;
   let baseLayer3Angle = PI / 6.78; 
 
-  groups.push(new lineGroup(baseLayer1StartX, baseLayer1StartY, baseLayer1EndX, baseLayer1EndY, baseLayer1Angle, 3, colorBase, 10, 3));
-  groups.push(new lineGroup(baseLayer2StartX, baseLayer2StartY, baseLayer2EndX, baseLayer2EndY, baseLayer2Angle, 3, colorBase, 10, 3));
-  groups.push(new lineGroup(baseLayer3StartX, baseLayer3StartY, baseLayer3EndX, baseLayer3EndY, baseLayer3Angle, 10, colorBase, 5, 3));
+  groups.push(new lineGroup(baseLayer1StartX, baseLayer1StartY, baseLayer1EndX, baseLayer1EndY, baseLayer1Angle, 3, colorBase, 10, 3, 0.1));
+  groups.push(new lineGroup(baseLayer2StartX, baseLayer2StartY, baseLayer2EndX, baseLayer2EndY, baseLayer2Angle, 3, colorBase, 10, 3, 0.2));
+  groups.push(new lineGroup(baseLayer3StartX, baseLayer3StartY, baseLayer3EndX, baseLayer3EndY, baseLayer3Angle, 10, colorBase, 5, 3, 0.3));
 }
 
 function drawMiddleLayer() {
@@ -119,15 +130,26 @@ function drawMiddleLayer() {
 }
 
 function drawTopLayer() {
-  fill(colorTop);
-  
   let topLayer1X1 = 0.398 * windowWidth;
   let topLayer1X2 = 0.166 * windowWidth;
   let topLayer1X3 = 0.398 * windowWidth;
   let topLayer1Y1 = 0.505 * windowHeight;
   let topLayer1Y2 = 0.711 * windowHeight;
-  let topLayer1Y3 = 0.919 * windowHeight;
-  triangle(topLayer1X1, topLayer1Y1, topLayer1X2, topLayer1Y2, topLayer1X3, topLayer1Y3);
+  let topLayer1Y3 = 0.919 * windowHeight; 
+
+  fill(colorTop, 20, 20); 
+  stroke(70);
+  
+  if (frameCount % 60 == 0) {
+	targetTriangleSize = random(height / 2);
+  }
+
+ // The new triangle size is calculated by linearly interpolating between the 
+  // last triangle size drawn and the target triangle size that gets updated
+  // every 60 frames
+  triangleSize = lerp(triangleSize, targetTriangleSize, easing);
+
+  triangle(topLayer1X1, triangleSize, topLayer1X2, topLayer1Y2, topLayer1X3, topLayer1Y3);
 
   let topLayer2X1 = 0.558 * windowWidth;
   let topLayer2X2 = 0.585 * windowWidth;
@@ -136,8 +158,8 @@ function drawTopLayer() {
   let topLayer2Y1 = 0.308 * windowHeight;
   let topLayer2Y2 = 0.283 * windowHeight;
   let topLayer2Y3 = 0.425 * windowHeight;
-  let topLayer2Y4 = 0.357 * windowHeight;
-  quad(topLayer2X1, topLayer2Y1, topLayer2X2, topLayer2Y2, topLayer2X4, topLayer2Y4, topLayer2X3, topLayer2Y3);
+  let topLayer2Y4 = 0.357 * windowHeight; 
+  quad(topLayer2X1, triangleSize, topLayer2X2, triangleSize, topLayer2X4, topLayer2Y4, topLayer2X3, topLayer2Y3);
 
   let topLayer3X1 = 0.558 * windowWidth;
   let topLayer3X2 = 0.558 * windowWidth;
@@ -149,9 +171,7 @@ function drawTopLayer() {
   let topLayer3Y4 = 0.403 * windowHeight;
   quad(topLayer3X1, topLayer3Y1, topLayer3X2, topLayer3Y2, topLayer3X3, topLayer3Y3, topLayer3X4, topLayer3Y4);
 }
-
-
-
+  
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   drawLineGroups();
